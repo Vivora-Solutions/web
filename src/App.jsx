06b56dafda632da910f29044682a1e5b4ad13bb9
Dulitha Perera <dulitha.22@cse.mrt.ac.lk>
2025-07-19 15:05:10 +0530

    Create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Header from './components/Header/Header';
 
 function App() {
 
   return (
     <div>
-      
+      <Header />
     </div>
   )
 }
