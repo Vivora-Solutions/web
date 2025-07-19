8cffade816313bb7b8757b28828438b20ee4b966
Dulitha Perera <dulitha.22@cse.mrt.ac.lk>
2025-07-19 15:43:02 +0530

    Create a basic super admin dashboard frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import Header from './components/Header/Header';
+import SuperAdminDashboard from './pages/SuperAdminDashboard/SuperAdminDashboard';
 
 function App() {
 
   return (
     <div>
       <Header />
+      <SuperAdminDashboard />
     </div>
   )
 }
