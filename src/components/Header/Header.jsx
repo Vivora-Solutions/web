8e6dc0244ad54f1802d887245e2a327fef900646
sahanweerasiri <smudunlahiru@gmail.com>
2025-08-02 16:36:03 +0530

    Refactor OpeningDays component for improved readability and performance; update styles in ServiceManagement CSS for better layout and aesthetics.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
   return (
-    <div className="flex justify-center items-center py-4">
-      <h1
-        className="text-center font-bold text-[2.5rem] bg-gradient-to-r from-[#0e12e2] via-[#19cef7] to-[#08c37e] bg-clip-text text-transparent animate-gradient font-italiana"
-      >
-        VIVORA
-      </h1>
-    </div>
+    <header className="header bg-white border-b border-gray-200 shadow-sm">
+      <div className="flex justify-center items-center py-4">
+        <h1
+          className="text-center font-bold text-[2.5rem] bg-gradient-to-r from-[#0e12e2] via-[#19cef7] to-[#08c37e] bg-clip-text text-transparent animate-gradient font-italiana"
+        >
+          VIVORA
+        </h1>
+      </div>
+    </header>
   )
 }
 
